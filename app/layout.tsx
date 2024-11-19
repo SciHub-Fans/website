e0c4ce200663b55fe3cee4cef42f8731efb8ea66
daxiongya <chongqiangchen@gmail.com>
2024-11-19 23:28:53 +0800

    feat: update info

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,29 @@ const sfMono = localFont({
 
 export const metadata: Metadata = {
   title: "$scihub UNLOCK SCIENCE",
-  description: "$scihub UNLOCK SCIENCE",
+  description: "$scihub is a community token dedicated to supporting the most influential science platform, Sci-Hub. Supporting the rebellion against paywalled knowledge, every $scihub token is a vote for open science.",
+  openGraph: {
+    title: "$scihub UNLOCK SCIENCE",
+    description: "$scihub is a community token dedicated to supporting the most influential science platform, Sci-Hub. Supporting the rebellion against paywalled knowledge, every $scihub token is a vote for open science.",
+    url: 'https://www.scihub.fans',
+    siteName: '$scihub',
+    images: [
+      {
+        url: '/images/social.jpeg',
+        width: 1200,
+        height: 630,
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "$scihub UNLOCK SCIENCE",
+    description: "$scihub is a community token dedicated to supporting the most influential science platform, Sci-Hub. Supporting the rebellion against paywalled knowledge, every $scihub token is a vote for open science.",
+    creator: '@SciHubFans',
+    images: ['/images/social.jpeg'],
+  },
 };
 
 export default function RootLayout({
