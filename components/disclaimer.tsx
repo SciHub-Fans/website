666661045a4c72bc6b25d9eaf1c13bd84dc3f036
GlacierLuo <1090490148@qq.com>
2024-11-19 20:20:37 +0800

    Fix disclaimer padding.

--- components/disclaimer.tsx
+++ components/disclaimer.tsx
@@ -1,6 +1,6 @@
 export const Disclaimer = () => {
     return (
-        <div className="flex w-full flex-col gap-2 justify-center [background:rgba(227,45,45,0.12)] backdrop-blur px-60 py-6 border-y-black border-t border-solid border-b mb-[48px]">
+        <div className="flex w-full flex-col gap-2 justify-center [background:rgba(227,45,45,0.12)] backdrop-blur px-4 md:px-8 lg:px-16 xl:px-60 py-6 border-y-black border-t border-solid border-b mb-[48px]">
             <p className="text-[#E32D2D]">DISCLAIMER:</p>
             <div className="flex flex-col gap-1">
                 <ol className="list-decimal pl-4">
