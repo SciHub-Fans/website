a4bf71a749d2fb78a6a80734e4928aeceb554a3f
daxiongya <chongqiangchen@gmail.com>
2024-11-19 20:31:16 +0800

    feat: 更新细节

--- components/animated-text.tsx
+++ components/animated-text.tsx
@@ -26,45 +26,12 @@ const AnimatedText = () => {
     return 0;
   };
 
-  const glowVariants = {
-    initial: {
-      background: "linear-gradient(90deg, rgba(255,255,255,0.12) 0%, rgba(255,255,255,0.12) 100%)",
-      boxShadow: "0px 0px 0px 0px rgba(255,255,255,0)",
-      border: "0.35px solid rgba(255,255,255,0)"
-    },
-    hover: {
-      background: [
-        "linear-gradient(90deg, rgba(255,255,255,0.12) -100%, rgba(255,255,255,0.3) 0%, rgba(255,255,255,0.12) 100%)",
-        "linear-gradient(90deg, rgba(255,255,255,0.12) 0%, rgba(255,255,255,0.3) 100%, rgba(255,255,255,0.12) 200%)",
-      ],
-      boxShadow: "0px 0px 13.98px 0px rgba(255,255,255,0.16)",
-      border: "0.35px solid rgba(255,255,255,0.5)",
-      transition: {
-        background: {
-          repeat: Infinity,
-          duration: 2,
-          ease: "linear",
-          repeatType: "reverse",
-        },
-        boxShadow: {
-          duration: 0.3
-        },
-        border: {
-          duration: 0.3
-        }
-      }
-    }
-  };
-
   return (
-    <motion.div
+    <div
       ref={containerRef}
-      variants={glowVariants}
-      initial="initial"
-      animate={isHovered ? "hover" : "initial"}
       className={cn(
-        "relative w-full inline-flex shrink-0 text-[20px] md:text-[44px] lg:text-[58px] xl:text-[80px] backdrop-blur-[20px] rounded-full items-center px-[32px] py-[22px] md:px-[69px] md:py-[48px] lg:px-[108px] lg:py-[64px] xl:py-[54px]",
-        "border-solid"
+        "relative w-full inline-flex shrink-0 backdrop-blur-[5px] text-[20px] md:text-[44px] lg:text-[58px] xl:text-[80px] rounded-full items-center px-[32px] py-[22px] md:px-[69px] md:py-[48px] lg:px-[108px] lg:py-[64px] xl:py-[54px]",
+        isHovered ? "[background:linear-gradient(-90deg,rgba(255,255,255,0.15)_0%,rgba(255,255,255,0.2)_50%,rgba(255,255,255,0.15)_100%)]" : "[background:rgba(255,255,255,0.12)]"
       )}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
@@ -77,7 +44,7 @@ const AnimatedText = () => {
       </span>
       <motion.div
         ref={slideRef}
-        className="absolute left-[3px] md:left-[6px] px-[22px] py-[20px] md:px-[67px] md:py-[42px] lg:px-[64px] lg:py-[57px] xl:py-[46px] lg:left-[7px] bg-black backdrop-blur-[20px] rounded-full flex items-center justify-center"
+        className="absolute left-[3px] md:left-[6px] px-[22px] py-[20px] md:px-[67px] md:py-[42px] lg:px-[64px] lg:py-[57px] xl:py-[46px] lg:left-[7px] bg-black backdrop-blur-[5px] rounded-full flex items-center justify-center"
         animate={{
           x: isHovered ? getSlideDistance() : 0,
         }}
@@ -91,7 +58,7 @@ const AnimatedText = () => {
           UNLOCK SCIENCE
         </span>
       </motion.div>
-    </motion.div>
+    </div>
   );
 };
 
