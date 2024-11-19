138bf683cd72438e815990f6ffb54eec9733479c
daxiongya <chongqiangchen@gmail.com>
2024-11-19 16:26:19 +0800

    feat: init

--- components/donation-progress.tsx
+++ components/donation-progress.tsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import { Icons } from "./icons";
 import Title from "./title";
-import Image from "next/image";
 
 const DonationProgress = () => {
   return (
