a039f564f2a1c345a8e8194ac7abe4cf3a4914fd
GlacierLuo <1090490148@qq.com>
2024-11-19 19:04:05 +0800

    Add index background picture.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import CopyFam from "@/components/copy-fam";
 export default function Home() {
   return (
     <div className="min-h-screen w-screen bg-black text-white">
-      <div className="container max-w-[1600px] mx-auto">
+      <div className="container max-w-[1600px] mx-auto bg-[url('/images/index-background.png')] bg-cover bg-center bg-no-repeat bg-fixed">
         <Header />
         <Hero />
         <Introduce />
