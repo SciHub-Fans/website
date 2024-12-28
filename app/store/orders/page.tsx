f1ebc2d031a8889da5dff1f8c9775d32c552b385
GlacierLuo <1090490148@qq.com>
2024-12-28 19:56:26 +0800

    feat: Add orders page.

---
+++ app/store/orders/page.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Header } from "@/features/store/components/header";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
+import { useQuery } from "@tanstack/react-query";
+import { getOrdersByWalletAddress } from "@/features/store/api/order";
+
+const StorePage = () => {
+    const { publicKey, sendTransaction, connected } = useWallet();
+    const { data: orders, isLoading } = useQuery({
+        queryKey: ["orders"],
+        queryFn: () => getOrdersByWalletAddress(publicKey?.toBase58() || ""),
+        enabled: connected && publicKey !== null,
+    });
+    return (
+        <div className="flex-1 sci-container relative z-1">
+            <Header />
+            {publicKey && <div className="flex-1 sci-container relative z-1">
+                <h1>My Orders</h1>
+                <Table>
+                    <TableCaption>Orders</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Order ID</TableHead>
+                            <TableHead>Product</TableHead>
+                            <TableHead>Total Amount</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Shiping Infomation</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {orders?.map((order, index) => (
+                            <TableRow key={index}>
+                                <TableCell>
+                                    <div>
+                                        <div>{order.id}</div>
+                                        <div className="text-xs text-[#A5A5A5]">{(new Date(order.createdAt)).toLocaleDateString()}</div>
+                                    </div>
+                                </TableCell>
+                                <TableCell>{order.orderProducts.map((product) => {
+                                    return (<div key={product.productId}>
+                                        <div>{product.name}</div>
+                                        <div>({product.variantValues.map(v => `${v.name} ${v.value}`).join(", ")}, {product.quantity} pc)</div>
+                                    </div>)
+                                })}</TableCell>
+                                <TableCell>{order.uiAmount} {order.currency}</TableCell>
+                                <TableCell>{order.status}</TableCell>
+                                <TableCell>
+                                    <div>
+                                        <div>Name: {order.firstName} {order.lastName}</div>
+                                        <div>Address: {order.address}</div>
+                                        <div>Postal Code: {order.zip}</div>
+                                        <div>Phone: {order.phone}</div>
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>}
+            {(!connected || !publicKey) && <div className="flex-1 sci-container relative z-1 text-center">
+                <h1>Please connect your wallet to view your orders</h1>
+            </div>}
+        </div>
+    )
+}
+
+export default StorePage;
