c7c32c6a404f6e381dc520487dcf9c411ebeb9c7
GlacierLuo <1090490148@qq.com>
2024-12-30 17:30:43 +0800

    fix: Fix an issue about storing login status.

--- features/auth/atom/auth.ts
+++ features/auth/atom/auth.ts
@@ -1,4 +1,14 @@
 import { atomWithStorage } from "jotai/utils";
 
-export const isLoginAtom = atomWithStorage<boolean>("isLogin", false);
+export const isLoginAtom = atomWithStorage<boolean>("isLogin", false, {
+    getItem: async (key, initialValue) => {
+        return localStorage.getItem(key) === "true" || initialValue;
+    },
+    setItem: async (key, value) => {
+        localStorage.setItem(key, value.toString());
+    },
+    removeItem: async (key) => {
+        localStorage.removeItem(key);
+    }
+}, {getOnInit: true});
 export const tokenAtom = atomWithStorage<string>("token", "");
